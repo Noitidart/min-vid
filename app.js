5e8eccc5ab0e1ebeec465d06ac1b174e0f574efa
Jared Hirsch <ohai@6a68.net>
2016-08-30 22:52:35 -0700

    Draggable + iframe working. Gets wobbly at points, though.
    Currently toggling an embed src after a three second delay after trying
    to 'send to mini player'. Works great!
    
    Seems like, if the mouse cursor makes it over top of the iframe while
    dragging, the panel jumps away from the cursor a little bit, resulting
    in a weird elliptical wobble if the mouse pointer moves too fast.
    
    Should be easy to fix: on drag start, stopImmediateProp for mouseenter events (or
    hover events or whatever the controls use to pop open). On drag stop,
    stop swallowing the mouse events.
    
    But yeah, dragging works so much better with XUL.

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ const ReactDOM = require('react-dom');
 const AppView = require('./components/app-view.js');
 
 const defaultData = {
-  id: 'L6_SbflSwAg',
-  src: 'https://r6---sn-n4v7sn7z.googlevideo.com/videoplayback?id=eb3f36c3497a9301&itag=160&source=youtube&requiressl=yes&nh=IgpwcjAyLnNqYzA3KgkxMjcuMC4wLjE&initcwndbps=1041250&mm=31&mn=sn-n4v7sn7z&pl=33&ms=au&mv=m&ratebypass=yes&mime=video/mp4&gir=yes&clen=2094587&lmt=1429521893147744&dur=153.486&upn=id8vRp4-rIo&signature=4FC644F013909D945D8D1AF51115D230FE669E02.064E053A74ECC1960E89F80293467CF6B19C799E&key=dg_yt0&sver=3&mt=1472619916&ip=2601:642:c303:1810:4c53:872c:78e9:c7ad&ipbits=0&expire=1472641978&sparams=ip,ipbits,expire,id,itag,source,requiressl,nh,initcwndbps,mm,mn,pl,ms,mv,ratebypass,mime,gir,clen,lmt,dur',
-  domain: 'youtube.com',
+  id: '',
+  src: '',
+  domain: '',
   minimized: false,
   loaded: false,
   error: false,
--- data/controls.js
+++ data/controls.js
@@ -15,9 +15,16 @@ self.port && self.port.on('set-video', opts => {
   unsafeWindow.AppData = Object.assign(unsafeWindow.AppData, opts);
 });
 
+// TODO: just until the WebCHannel is wired up, auto-toggle the src
+setTimeout(() => {
+  console.log('time is up, data/controls.js about to reset the src');
+  // TODO: where did unsafeWindow go?
+  AppData.src = 'https://www.youtube.com/embed/dYFALyP2e7U/?autoplay=0&modestbranding=1&controls=0&disablekb=0&enablejsapi=1&fs=0&iv_load_policy=3&loop=0&rel=0&showinfo=0';
+}, 3000);
+
 // Bridge between app.js window messages to the
 // addon. We pass true for the wantsUntrusted param
 // in order to access the message events. #82
 window.addEventListener('addon-message', function(ev) {
-  self.port.emit('addon-message', ev.detail);
+  self.port && self.port.emit('addon-message', ev.detail);
 }, false, true);
