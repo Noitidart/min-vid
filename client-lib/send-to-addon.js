4d582fa100f285b0d8177e08140786e0c44645d5
Jared Hirsch <ohai@6a68.net>
2016-10-12 09:09:12 -0700

    Pretty sure it is now working, except need to move drag handle into commands, not make close button draggable

--- client-lib/send-to-addon.js
+++ client-lib/send-to-addon.js
@@ -1,7 +1,6 @@
 module.exports = sendToAddon;
 
 function sendToAddon(obj) {
-  throw new Error('sendToAddon: ', obj);
   // TODO: overwrites if multiple commands issued. should work fine.
   window.pendingCommand = JSON.stringify(obj);
 }
